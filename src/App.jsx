0d6600f03d2965a499987ed016250fc3b61a8542
Marcos Vinicius <marcosviniciusmoreira001@gmail.com>
2021-09-28 10:40:29 -0300

    teste tailwind-4

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Teste from './Componentes/Teste';
 const App = () => {
   return ( 
     <>
-    <div className="bg-gray-600 p-10 sm:bg-pink-600 md:bg-blue-600 lg:bg-purple-600 xl:bg-pink-600 ">
+    <div className="bg-gray-600 p-10 sm:bg-pink-600 md:bg-blue-600 lg:bg-purple-600 xl:bg-pink-600">
       <Teste />
 
       <div className="block bg-green-600 text-white text-center p-2 mt-4 rounded-2xl mx-8 hover:bg-green-400 delay-100 duration-500 ease-linear md:mx-8 lg:mx-0">
         <a 
-         href="#">Quero mais q vc se foda
+         href="#">proximo
         </a>
       </div>
     </div>
--- src/Componentes/Teste.jsx
+++ src/Componentes/Teste.jsx
@@ -4,13 +4,13 @@ import img from '../Imgs/add.jpg'
 const Teste = () => {
     return ( 
         <>
-        <div className="bg-gray-200 p-8 rounded-3xl md:">
+        <div className="bg-gray-200 p-8 rounded-3xl overflow-hidden md:flex md:p-0 ">
             <div>
-            <img className="w-32 h-32 rounded-full mx-auto " src={img} alt=""/>
+            <img className="w-32 h-32  rounded-full mx-auto sm:h-full md:max-w-lg   md:rounded-none" src={img} alt=""/>
             </div>
 
             <div className="pt-6 text-center text-base space-y-4">
-                <div className="m-0 md:mx-56">
+                <div className="m-0 md:">
                     <p>
                         "Quando eu comecei o curso, eu sabia nada de nada
                         mas depois de um ano estudando quando tinha tempo
